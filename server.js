e86b29b2f7bca47ef82b53ef8624620505fa4191
Gustavo <lpc9730@gmail.com>
2021-08-09 22:14:20 -0500

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaatitaniza

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/Frontend'));
+app.use(express.static('./dist/mondongo'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Frontend/'}),
+    res.sendFile('index.html', {root: 'dist/mondongo/'}),
 );
 
 // Start the app by listening on the default Heroku port
