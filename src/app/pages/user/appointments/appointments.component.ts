016fd572d4394b273bccfae9f6020968b7962f89
Gustavo <lpc9730@gmail.com>
2021-08-16 10:33:26 -0500

    funcionalidades plus

--- src/app/pages/user/appointments/appointments.component.ts
+++ src/app/pages/user/appointments/appointments.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { Specialty } from '../../../core/models/specialty.models';
 import { Medic } from '../../../core/models/medic.models';
 import { ENSUREDS_DATA } from '../../../mock/ensured.mock';
@@ -20,11 +20,11 @@ export class AppointmentsComponent implements OnInit {
   age!: number;
   id_history!: string;
   enabled: boolean = false;
-  controlDni = new FormControl('',[]);
-  controlSpecialty = new FormControl('',[]);
-  controlMedic = new FormControl('',[]);
-  controlDate = new FormControl('',[]);
-  controlSchedule = new FormControl('',[]);
+  controlDni = new FormControl('',[Validators.required, Validators.maxLength(8)]);
+  controlSpecialty = new FormControl('',[Validators.required]); 
+  controlMedic = new FormControl('',[Validators.required]);
+  controlDate = new FormControl('',[Validators.required]);
+  controlSchedule = new FormControl('',[Validators.required]);
   specialties: Specialty[] = SPECIALTIES_DATA;
   medics!: Medic[];
   schedules: string[] = [];
@@ -68,16 +68,28 @@ export class AppointmentsComponent implements OnInit {
   }
 
   generateAppointment(){
-    let id:string = "";
-    APPOINTMENTS_DATA.push({
-      id: id.concat("AP",(APPOINTMENTS_DATA.length+1).toString()),
-      dni_ensured:this.controlDni.value,
-      id_specialty: this.controlSpecialty.value,
-      id_medic:this.controlMedic.value,
-      appointment_date: this.controlDate.value,
-      schedule:this.controlSchedule.value
-    })
-    console.log(APPOINTMENTS_DATA);
+    let id: string = "";
+    if(this.dni!=null && 
+       this.controlDate.valid && 
+       this.controlMedic.valid && 
+       !(this.controlMedic.value == "Seleccione") &&
+       this.controlSpecialty.valid &&
+       !(this.controlSpecialty.value == "Seleccione") &&
+       this.controlSchedule.valid &&
+       !(this.controlSchedule.value == "Seleccione")){
+        APPOINTMENTS_DATA.push({
+          id: id.concat("AP",(APPOINTMENTS_DATA.length+1).toString()),
+          dni_ensured:this.dni,
+          id_specialty: this.controlSpecialty.value,
+          id_medic:this.controlMedic.value,
+          appointment_date: this.controlDate.value,
+          schedule:this.controlSchedule.value
+        })
+        alert('Cita generada')
+       }else{
+         alert('Complete todos los campos')
+       }
+    console.log(APPOINTMENTS_DATA.length);
     console.log(this.controlDate.value)
   }
 }
